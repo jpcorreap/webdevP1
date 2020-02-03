6b3289f4877da458a34810e0dc2dbdbdbe11955f
Juan Pablo Correa Puerta <juanpablocorreapuerta@gmail.com>
2020-02-03 06:46:12 -0500

    Javascript requirement completed

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
-function sayHello() {
-  alert( "Hello!" );
 
-function changeColors() {
-  document.getElementByClass("title").style.color = "blue";
+/**
+* This function allows change colors of navbar and title
+*/
+function changeRandomColors() {
+  // Generates random hex color
+  let red = rgbToHex( Math.floor((Math.random() * 255) + 1) );
+  let green = rgbToHex( Math.floor((Math.random() * 255) + 1) );
+  let blue = rgbToHex( Math.floor((Math.random() * 255) + 1) );
+  let randomColor = "#" + red + green + blue;
+
+  // Change colors of navbar and title
+  document.getElementById('navbar').style['background-color'] = randomColor;
+  document.getElementById('name').style['color'] = randomColor;
 }
+
+// Original code from https://campushippo.com/lessons/how-to-convert-rgb-colors-to-hexadecimal-with-javascript-78219fdb
+var rgbToHex = function (rgb) { 
+  var hex = rgb.toString(16);
+  if (hex.length < 2) {
+       hex = "0" + hex;
+  }
+  return hex;
+};
+
+
+
